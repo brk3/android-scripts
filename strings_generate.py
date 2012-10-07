b8942faba852be4d9fad8d7081c88ca640dffad4
Paul Bourke <pauldbourke@gmail.com>
2012-10-07 17:17:55 +0100

    Fix ellipses and check for missing linebreaks

--- strings_generate.py
+++ strings_generate.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: utf8 -*-
 
 from lxml import etree
 import codecs
@@ -13,10 +14,21 @@ if len(sys.argv) != 3:
     print('Usage: strings_generate.py <strings.xml> <translations>')
     sys.exit(1)
 
+def clean_and_check(element):
+    if element.text == '':
+        return ''
+    if '\\n' in original_strings[count] and not '\\n' in element.text:
+        print("Warning! '%s' contains linebreaks but translation '%s' "
+              "does not" % (original_strings[count], element.text))
+    element.text = element.text.replace('...', u'…')
+    element.text = element.text.replace('..', u'…')
+    return element.text
+
 xml_file = sys.argv[1]
 trans_file = sys.argv[2]
 
 tags = []
+original_strings = []
 translations = []
 
 # Load the tags
@@ -25,6 +37,7 @@ with open(xml_file, 'r') as input:
 _myxml = [i.lstrip() for i in myxml]
 root = etree.fromstring(' '.join(_myxml))
 for i in root.findall('string'):
+    original_strings.append(i.text)
     tags.append(i.attrib['name'])
 
 # Load the translations
@@ -40,8 +53,7 @@ root = etree.Element("resources")
 for count, tag in enumerate(tags):
     e = etree.Element("string", name=tag)
     e.text = translations[count]
-    # don't add empty entries, android should fall back to english translation
-    if e.text == '': continue
+    if not clean_and_check(e): continue
     root.append(e)
 
 with codecs.open('/tmp/output.xml', encoding='utf-8', mode='w') as outfile:
